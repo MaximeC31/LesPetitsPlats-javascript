be0c2c7cc9292f3aa0056352d8278b3461bd2377
Maxime <contact@maximechampigneux.fr>
2024-09-24 23:01:49 +0200

    api-method-prototype-array

--- assets/js/utils/api.js
+++ assets/js/utils/api.js
@@ -2,80 +2,43 @@ import { getQueryParams } from './queryParamsHandler.js';
 import { recipes } from '../../../data/recipes.js';
 
 export const getRecipesIngredients = () => {
-	const allIngredients = [];
-	for (const recipe of recipes) {
-		for (const ingredient of recipe.ingredients) {
-			if (!allIngredients.includes(ingredient.ingredient)) {
-				allIngredients.push(ingredient.ingredient);
-			}
-		}
-	}
-	return allIngredients;
+	const allIngredients = recipes.flatMap((recipe) =>
+		recipe.ingredients.map((ingredient) => ingredient.ingredient),
+	);
+	return [...new Set(allIngredients)];
 };
 
 export const getRecipesAppliances = () => {
-	const allAppliances = [];
-	for (const recipe of recipes) {
-		if (!allAppliances.includes(recipe.appliance)) {
-			allAppliances.push(recipe.appliance);
-		}
-	}
-	return allAppliances;
+	const allAppliances = recipes.flatMap((recipe) => recipe.appliance);
+	return [...new Set(allAppliances)];
 };
 
 export const getRecipesUstensils = () => {
-	const allUstensils = [];
-	for (const recipe of recipes) {
-		for (const ustensil of recipe.ustensils) {
-			if (!allUstensils.includes(ustensil)) {
-				allUstensils.push(ustensil);
-			}
-		}
-	}
-	return allUstensils;
+	const allUstensils = recipes.flatMap((recipe) => recipe.ustensils);
+	return [...new Set(allUstensils)];
 };
 
 export const getFilteredIngredients = (recipe, ingredientsParams) => {
 	if (ingredientsParams.length === 0) {
 		return true;
 	}
-	for (const ingredient of ingredientsParams) {
-		let found = false;
-		for (const recipeIngredient of recipe.ingredients) {
-			if (recipeIngredient.ingredient === ingredient) {
-				found = true;
-				break;
-			}
-		}
-		if (!found) {
-			return false;
-		}
-	}
-	return true;
+	return ingredientsParams.every((ingredient) =>
+		recipe.ingredients.some((recipeIngredient) => recipeIngredient.ingredient === ingredient),
+	);
 };
 
 export const getFilteredAppliances = (recipe, appliancesParams) => {
 	if (appliancesParams.length === 0) {
 		return true;
 	}
-	for (const appliance of appliancesParams) {
-		if (!recipe.appliance.includes(appliance)) {
-			return false;
-		}
-	}
-	return true;
+	return appliancesParams.every((appliance) => recipe.appliance.includes(appliance));
 };
 
 export const getFilteredUstensils = (recipe, ustensilsParams) => {
 	if (ustensilsParams.length === 0) {
 		return true;
 	}
-	for (const ustensil of ustensilsParams) {
-		if (!recipe.ustensils.includes(ustensil)) {
-			return false;
-		}
-	}
-	return true;
+	return ustensilsParams.every((ustensil) => recipe.ustensils.includes(ustensil));
 };
 
 export const getFilteredServices = (recipe, searchParam) => {
@@ -100,16 +63,14 @@ export const getFilteredServices = (recipe, searchParam) => {
 };
 
 export const getFilteredRecipes = (queryParams = getQueryParams()) => {
-	const filteredRecipes = [];
-	for (const recipe of recipes) {
-		if (
+	const filteredRecipes = recipes.filter((recipe) => {
+		return (
 			getFilteredIngredients(recipe, queryParams.ingredients) &&
 			getFilteredAppliances(recipe, queryParams.appliances) &&
 			getFilteredUstensils(recipe, queryParams.ustensils) &&
 			getFilteredServices(recipe, queryParams.search)
-		) {
-			filteredRecipes.push(recipe);
-		}
-	}
+		);
+	});
+
 	return filteredRecipes;
 };
